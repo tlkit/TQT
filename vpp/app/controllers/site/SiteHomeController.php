fa6d03966a2d6e955ab4b0ad2ec89e4036f3f4e2
quynhtm <quynhtruongmanh@vccorp.vn>
2015-07-01 16:40:20 +0700

    sua

--- vpp/app/controllers/site/SiteHomeController.php
+++ vpp/app/controllers/site/SiteHomeController.php
@@ -28,9 +28,27 @@ class SiteHomeController extends BaseController {
         die('Đây là trang chủ');
     }
 
-    public function offSite()
-    {
+    public function offSite(){
         $url_src_icon = URL::to('/').'/images/cap-nhat-va-bao-tri-web.jpg';
-        return View::make('site.offSite')->with('url_src_icon',$url_src_icon);
+        $today = time();
+        $end_time = '31-08-2015';//d/m/Y
+        $deline = strtotime($end_time);
+        $date_off = $this->timeLifeWithToday($today,$deline);
+        return View::make('site.offSite')->with('url_src_icon',$url_src_icon)->with('date_off',$date_off);
+    }
+
+    function timeLifeWithToday($startDay, $endDay) {
+        $date_start = date('Y-m-d', $startDay);
+        $date_end = date('Y-m-d', $endDay);
+        $diff_start = abs(strtotime($date_end) - strtotime($date_start));
+        //$khoang_cach_date = $this->getDateToDate($diff_start);
+        return $diff_start;
+    }
+
+    function getDateToDate($diff) {
+        $years = floor($diff / (365 * 60 * 60 * 24));
+        $months = floor(($diff - $years * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
+        $days = floor(($diff - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24) / (60 * 60 * 24));
+        return $days;
     }
 }
