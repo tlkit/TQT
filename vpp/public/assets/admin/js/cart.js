a4c67fa60ca21f8e825cbe1b9d1a717b70761ab4
tlkit <tlkit158@gmail.com>
2016-07-05 15:51:35 +0700

    xxxx

--- vpp/public/assets/admin/js/cart.js
+++ vpp/public/assets/admin/js/cart.js
@@ -71,7 +71,7 @@ var AdminCart = {
         });
         directionsDisplay.setMap(map);
         directionsDisplay.setPanel($('#panel')[0]);
-        var request = {
+        /*var request = {
             origin: start,
             destination: end,
             travelMode: google.maps.DirectionsTravelMode.DRIVING
@@ -81,6 +81,40 @@ var AdminCart = {
                 directionsDisplay.setDirections(response);
             }
         });
+*/
+        ///
+        var waypts = [
+            {location:'số 1 nguyễn huy tưởng thanh xuân hà nội',stopover:true},
+            {location:'số 10 thái hà hà nội',stopover:true},
+            {location:'số 4 phương mai đồng đa hà nội',stopover:true}
+        ]
+
+        directionsService.route({
+            origin: start,
+            destination: start,
+            waypoints: waypts,
+            optimizeWaypoints: true,
+            travelMode: google.maps.TravelMode.DRIVING
+        }, function(response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                var route = response.routes[0];
+                var summaryPanel = document.getElementById('directions-panel');
+                summaryPanel.innerHTML = '';
+                // For each route, display summary information.
+                /*for (var i = 0; i < route.legs.length; i++) {
+                    var routeSegment = i + 1;
+                    summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                    '</b><br>';
+                    summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                    summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                    summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+                }*/
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+
 
     },
     hiddenMap : function(){
